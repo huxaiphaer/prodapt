f144c341623471fd79b98d074e43f2319496872f
Lutaaya Idris <lutaaya.idris@captiq.com>
2022-10-10 02:51:47 +0300

    Ft add models

--- weather/models.py
+++ weather/models.py
@@ -1,3 +1,22 @@
+import uuid as uuid
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+from django_extensions.db.models import TimeStampedModel
 
-# Create your models here.
+
+class WeatherForecast(TimeStampedModel, models.Model):
+
+    """Weather forecast model."""
+
+    uuid = models.UUIDField(unique=True, max_length=500,
+                            default=uuid.uuid4,
+                            editable=False,
+                            db_index=True, blank=False, null=False)
+    city = models.CharField(_('City'), max_length=100, null=True, blank=True)
+    description = models.TextField(_('Description'), null=True, blank=True)
+    date_time_forecasted = models.DateTimeField(
+        _('Date Time Forecasted'), null=True, blank=True)
+
+    def __str__(self):
+        """Model Representation."""
+        return self.city
